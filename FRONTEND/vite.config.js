0f212e48dff96fca3c3161e5bb943a1024f7f482
BethTawiah <elizabeth.tawiah@amalitech.org>
2025-11-17 22:35:12 +0000

    vite

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
-  
-})
+  preview: {
+    port: process.env.PORT || 4173, // Render sets the PORT automatically
+    host: true,                      // Listen on all network interfaces
+    allowedHosts: [
+      "real-view-estate-frontend.onrender.com" // Whitelist your deployed frontend
+    ],
+  },
+});
